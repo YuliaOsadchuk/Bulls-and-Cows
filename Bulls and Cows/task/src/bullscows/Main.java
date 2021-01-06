8944c130210a91a4be663b1f74f68d50a93a191a
Yulia Osadchuk <yulia.osadchuk@gmail.com>
2021-01-06 22:20:57 +0200

    add Stage 7/7: Error_

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -12,17 +12,46 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Input the length of the secret code:");
         Scanner scanner = new Scanner(System.in);
-        int count = scanner.nextInt();
+        int count = 0;
+        int numberPossible = 0;
+        String s = "";
+        try {
+            s = scanner.nextLine();
+            count = Integer.parseInt(s);
+        } catch (Exception e) {
+            System.out.println("Error: \"" + s + "\" isn't a valid number.");
+            return;
+        }
         if (count > 36) {
-            System.out.println("Error: can't generate a secret number with a length of " + count +
-                    " because there aren't enough unique digits.");
+            System.out.println("Error: it's not possible to generate a code with a length of 6 with 5 unique symbols.");
             return;
         }
         String[] symbols = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f",
                 "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 
         System.out.println("Input the number of possible symbols in the code:");
-        int numberPossible = scanner.nextInt();
+        try {
+            s = scanner.nextLine();
+            numberPossible = Integer.parseInt(s);
+        } catch (Exception e) {
+            System.out.println("Error: \"" + s + "\" isn't a valid number.");
+            return;
+        }
+
+        if (numberPossible > 36) {
+            System.out.println("Error");
+            return;
+        }
+
+        if (count == 0) {
+            System.out.println("Error");
+        }
+
+        if (numberPossible < count) {
+            System.out.println("Error: it's not possible to generate a code with a length of " + count + " with " +
+                    numberPossible + " unique symbols.");
+            return;
+        }
         Random random = new Random();
         StringBuilder stringBuilder = new StringBuilder();
         while (stringBuilder.length() != count) {
